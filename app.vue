e031d64ee7b298f2a9a5005aee7e3dead65c43b8
Petr Zenin <mr@adminoid.com>
2022-02-12 22:36:29 +0300

    initial commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<script setup>
+const counter = useState('counter', () => Math.round(Math.random() * 1000))
+</script>
+
+<template>
+  <div>
+    Counter: {{ counter }}
+    <button @click="counter++">
+      +
+    </button>
+    <button @click="counter--">
+      -
+    </button>
+  </div>
+</template>
