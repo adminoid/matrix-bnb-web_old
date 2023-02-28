1d3462833e8e022e23e63249d0123a89e33c49be
Petr Zenin <mr@adminoid.com>
2023-02-28 16:10:34 +0300

    configured for bsc testnet

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,10 +5,10 @@ export default defineNuxtConfig({
         public: {
             // CHAIN_ID: (process.env) ? process.env.CHAIN_ID : `0x61`,
 
-            // CHAIN_ID: `0x61`, // testnet - 97 in decimal
+            CHAIN_ID: `0x61`, // testnet - 97 in decimal
 
             // hardhat localhost
-            CHAIN_ID: `0x7A69`, // hardhat node
+            // CHAIN_ID: `0x7A69`, // hardhat node
 
             // CHAIN_ID: `1337`,
             // CHAIN_ID: `97`,
@@ -17,38 +17,35 @@ export default defineNuxtConfig({
             //     'https://data-seed-prebsc-1-s1.binance.org:8545/',
 
             // this testnet:
-            // RPC_URL: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
-            // I don't sure what is it:
-            // RPC_URL: 'https://data-seed-prebsc-2-s3.binance.org:8545/',
+            RPC_URL: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
 
             // this localhost hardhat
-            RPC_URL: 'http://127.0.0.1:8545',
+            // RPC_URL: 'http://127.0.0.1:8545',
 
             // RPC_URL: (process.env) ?
             //     process.env.RPC_URL :
             //     'https://data-seed-prebsc-2-s3.binance.org:8545/',
-            CHAIN_NAME: 'HardHat Network',
-            // CHAIN_NAME: 'Smart Chain - testnet',
-            // CONTRACT_ADDRESS: '0x2636FdEa1D061Bc13C2fa51CEf2366bF291aa6a5',
+            // CHAIN_NAME: 'HardHat Network',
+            CHAIN_NAME: 'Smart Chain - testnet',
 
             // hardhat localhost:
-            CONTRACT_ADDRESS: '0x5fbdb2315678afecb367f032d93f642f64180aa3',
+            // CONTRACT_ADDRESS: '0x5fbdb2315678afecb367f032d93f642f64180aa3',
 
             // testnet bsc
             // todo: !important - update this
-            // CONTRACT_ADDRESS: '0x4FF819B15Cfe2b70d309D87c5228eD22A64BEd32',
-
-            // CURRENCY: {
-            //     name: 'Binance Coin',
-            //     symbol: 'BNB',
-            //     decimals: 18,
-            // },
+            CONTRACT_ADDRESS: '0xc3DF0b1584F33f7aa8a628C970A1162F4BdEC011',
 
             CURRENCY: {
-                name: 'Hardhat coin',
-                symbol: 'HH',
+                name: 'Binance Coin',
+                symbol: 'BNB',
                 decimals: 18,
             },
+
+            // CURRENCY: {
+            //     name: 'Hardhat coin',
+            //     symbol: 'HH',
+            //     decimals: 18,
+            // },
         }
     },
     vite: {
